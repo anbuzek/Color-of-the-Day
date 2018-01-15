04087439f3e8ee063941d81099be508d6dd1a43e
anbuzek <35462153+anbuzek@users.noreply.github.com>
2018-01-15 11:33:39 -0500

    Add files via upload
    basic text code

---
+++ send_text.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "ACe78878762b0fefe6f0896c396683f833"
+# Your Auth Token from twilio.com/console
+auth_token  = "72c384d70e619a8a5b07fa420bc8a527"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+17039288450", 
+    from_="+12028167494",
+    body="Hello from Python!")
+
+print(message.sid)
