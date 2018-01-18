bd6de9e7f97fa43ec6dd468fcc6c64331ab73d72
Paulo Black <pblack@goodwaygroup.com>
2018-01-17 22:56:41 -0500

    refactor to functions

--- send_text.py
+++ send_text.py
@@ -1,27 +1,44 @@
+# When you import something, you are
 from twilio.rest import Client
-import os
-import random
+from dotenv import load_dotenv
+from random import choice
+from os import path, environ
 
-# Your Account SID from twilio.com/console
-myArray = []
+def get_twilio_client():
+    # Get the path to the current directory
+    app_dir = path.dirname(__file__)
 
-with open("/Users/pblack/code/sandbox/aaron/Color-of-the-Day/color_list.txt","r",encoding='utf-8', errors='ignore') as f:
-        for line in f:
+    # The dotenv library will look for the file passed in and load these variables into the environment
+    # Ask about what the environment and environment variables are!
+    load_dotenv("".join([app_dir, ".env"]))
 
-            if not line.strip():
-                pass
-            else:
-                myArray.append(line.rstrip('\r\n'))
+    auth_token = environ.get('AUTH_TOKEN')
+    account_sid = environ.get('ACCOUNT_SID')
 
-# Your Auth Token from twilio.com/console
-auth_token  = "72c384d70e619a8a5b07fa420bc8a527"
-account_sid = "ACe78878762b0fefe6f0896c396683f833"
-client = Client(account_sid, auth_token)
+    client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+17039288450",
-    from_="+12028167494",
-    body=random.choice(myArray)
-    )
+    return client
 
-print(message.sid)
+def parse_color_file():
+    colors = []
+    with open("/Users/pblack/code/sandbox/aaron/Color-of-the-Day/color_list.txt","r",encoding='utf-8', errors='ignore') as f:
+            for line in f:
+
+                if not line.strip():
+                    pass
+                else:
+                    colors.append(line.rstrip('\r\n'))
+
+    return colors
+
+
+def send_text(client,random_color):
+    message = client.messages.create(
+        to="+17039288450",
+        from_="+12028167494",
+        body=random_color
+        )
+
+colors = parse_color_file()
+client = get_twilio_client()
+send_text(client, choice(colors))
